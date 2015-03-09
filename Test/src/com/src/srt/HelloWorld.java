2be2ff3597624fc36541cc7de9c2912d3af4b9fa
Dhanraj Choudhary <dhanraj.choudhary@searshc.com>
2015-03-09 15:40:49 -0500

    First commit. Testing to push the project at remote repository

---
+++ Test/src/com/src/srt/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.src.srt;
+
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+
+public class HelloWorld {
+
+    private static final Logger LOGGER=Logger.getLogger(HelloWorld.class.getName());
+    public static void main(String args[])
+    {
+        LOGGER.info("Hello World");
+        
+    }
+    
+}
